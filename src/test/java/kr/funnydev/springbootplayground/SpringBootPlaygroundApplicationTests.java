fed24b18d653d4e39f2670f88d33b93e4749d970
linuxp <linuxp@naver.com>
2020-11-30 22:02:27 +0900

    유닛 테스트 작성 테스트

--- src/test/java/kr/funnydev/springbootplayground/SpringBootPlaygroundApplicationTests.java
+++ src/test/java/kr/funnydev/springbootplayground/SpringBootPlaygroundApplicationTests.java
@@ -1,6 +1,6 @@
 package kr.funnydev.springbootplayground;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
